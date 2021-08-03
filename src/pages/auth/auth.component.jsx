118548af2b39601a74b685d17141e4f1be0643fe
Irina <iratushina17@gmail.com>
2021-08-03 08:56:15 +0100

    Sign in component UI added

---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./auth.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const Auth = () => {
+    return (
+        <div className="sign-in-and-sign-up">
+            <SignIn />
+        </div>
+    )
+}
+
+export default Auth;
